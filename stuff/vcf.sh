a72e6d8eaff6f3dc4071cc7a454c6d13359e5fe8
iceyrazor <iceyfoxservers@gmail.com>
2024-03-06 22:15:46 -0600

    vcf fix :|

--- stuff/vcf.sh
+++ stuff/vcf.sh
@@ -2,21 +2,21 @@
 #see https://www.vseeface.icu/#running-on-linux-and-maybe-mac
 
 if [ "$(pstree -pla | grep facetracker.py | sed '/grep/d')" == "" ]; then
-    printf "starting facetrack.py"
+    printf "starting facetrack.py\n"
     cd ~/stuff/manual-programs/OpenSeeFace
     source env/bin/activate
-    nohup python facetracker.py -c 0 -W 1280 -H 720 --discard-after 0 --scan-every 0 --no-3d-adapt 1 --max-feature-updates 900 & disown
+    nohup python facetracker.py -c 0 -W 1280 -H 720 --discard-after 0 --scan-every 0 --no-3d-adapt 1 --max-feature-updates 900 > /dev/null 2> /dev/null < /dev/null & disown
 fi
 
 if [ "$(pstree -pla | grep VSeeFace.exe | sed '/grep/d')" == "" ]; then
-    printf "starting VSeeFace"
+    printf "starting VSeeFace\n"
     cd ~/.wine/drive_c/VSeeFace
-    wine64 ./VSeeFace.exe & disown
+    nohup wine64 ./VSeeFace.exe > /dev/null 2> /dev/null < /dev/null & disown
 fi
 
+printf "\n\n"
 read -p "kill facetrack?"
-
-pids=$(pstree -pla | grep facetracker.py | sed 's,|,,g' | sed 's/  //g' | sed 's/^ //g' | sed 's/ .*//' | sed 's/[^0-9]*//g')
+pids=$(pstree -pla | grep facetracker.py | sed '/grep/d' | sed 's,|,,g' | sed 's/  //g' | sed 's/^ //g' | sed 's/ .*//' | sed 's/[^0-9]*//g')
 
 echo $pids
 kill $pids
