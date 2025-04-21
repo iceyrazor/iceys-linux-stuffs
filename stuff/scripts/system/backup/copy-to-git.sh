88a57ecefe32352104d1bba13ba08af02f27fc9b
iceyrazor <iceyfoxservers@gmail.com>
2025-04-21 17:57:09 -0500

    phfuck

--- stuff/scripts/system/backup/copy-to-git.sh
+++ stuff/scripts/system/backup/copy-to-git.sh
@@ -11,7 +11,7 @@ fsync () {
     fi
 }
 
-syncfile=".urlview .vimrc .Xresources .zshrc .p10k.zsh .config/.profile .config/wezterm .config/dwm .config/tmux .config/awesome .config/rofi .config/lf .config/picom.conf .config/nvim .config/atuin .config/screenkey.json stuff/manual-programs/suckless"
+syncfile=".urlview .vimrc .Xresources .bashrc .zshrc .p10k.zsh .config/.profile .config/wezterm .config/dwm .config/tmux .config/awesome .config/rofi .config/lf .config/picom.conf .config/nvim .config/atuin .config/screenkey.json stuff/manual-programs/suckless"
 
 syncfile="$syncfile stuff/scripts/system/stbar stuff/scripts/system/url-handler.sh stuff/scripts/system/backup/copy-to-git.sh stuff/scripts/system/backup/backup.sh"
 syncfile="$syncfile stuff/scripts/system/neoboot.sh stuff/scripts/system/math.sh stuff/scripts/system/bri.sh stuff/scripts/system/restart-pipe.sh stuff/scripts/system/set-pri-java.sh"
