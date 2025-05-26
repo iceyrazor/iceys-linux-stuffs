571effe7375fa6c21b00fa32b40010d6353eb471
iceyrazor <iceyrazor@mailfence.com>
2025-05-26 15:19:57 -0500

    added mimes

---
+++ runs/mimes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo SETTING MIMES
+unset BROWSER
+xdg-mime default zen.desktop application/pdf
+xdg-mime default pinta.desktop image/jpeg
+xdg-mime default pinta.desktop image/png
+xdg-mime default pinta.desktop image/webp
+xdg-settings set default-web-browser custom-zen.desktop
+echo --------
